6993d6f79e6e3a1bb99591d94ff9f3f672f867df
Ayoub <ayoub.ber99@gmail.com>
2025-05-08 13:03:16 +0100

    Update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from narrator import extract_text_from_pptx, text_to_speech
-
+import os
 st.title("Narrateur vocal de présentation PowerPoint")
 st.markdown("""
     ### Instructions
@@ -9,12 +9,15 @@ st.markdown("""
     3. Vous pourrez écouter chaque diapositive en cliquant sur le lecteur audio.
 """)
 uploaded_file = st.file_uploader("Uploader un fichier .pptx", type=["pptx"])
+# Définir le répertoire de sortie
+output_dir = "output"
+# clean_output_directory(output_dir)  # Nettoyer le répertoire de sortie
 if uploaded_file:
     slides = extract_text_from_pptx(uploaded_file)
     for i, slide in enumerate(slides):
         st.markdown(f"### Diapositive {i+1}")
         st.text(slide)
-        output_file = f"audio_slide_{i+1}.mp3"
+        output_file = os.path.join(output_dir, f"audio_slide_{i+1}.mp3")
         text_to_speech(slide, output_file)
         st.audio(output_file)
 
