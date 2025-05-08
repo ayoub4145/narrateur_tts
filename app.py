07904bc73659344f6691ed3c46c8786eb7d2f61a
Ayoub <ayoub.ber99@gmail.com>
2025-05-08 12:48:58 +0100

    `Added instructions and author information to app.py`

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import streamlit as st
 from narrator import extract_text_from_pptx, text_to_speech
 
 st.title("Narrateur vocal de présentation PowerPoint")
-
+st.markdown("""
+    ### Instructions
+    1. Uploadez un fichier PowerPoint (.pptx).
+    2. Le texte de chaque diapositive sera extrait et converti en audio.
+    3. Vous pourrez écouter chaque diapositive en cliquant sur le lecteur audio.
+""")
 uploaded_file = st.file_uploader("Uploader un fichier .pptx", type=["pptx"])
 if uploaded_file:
     slides = extract_text_from_pptx(uploaded_file)
@@ -12,3 +17,6 @@ if uploaded_file:
         output_file = f"audio_slide_{i+1}.mp3"
         text_to_speech(slide, output_file)
         st.audio(output_file)
+
+
+st.markdown("Developpé par [Ayoub BERHILI](https://github.com/ayoub4145)")
